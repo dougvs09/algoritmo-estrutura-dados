ba1d0bdcef62ac407a939246ccb0eb9bd4fe4ec1
dougvs09 <douglasvalente6@gmail.com>
2025-03-19 11:29:04 -0300

    add bfs and dfs search, add inorder traversal, add preorder traversal and add postorder traversal

--- binary_tree/BinaryTree.java
+++ binary_tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package binary_tree;
 
+import java.util.*;
+
 public class BinaryTree {
 
     private TreeNode root;
@@ -51,10 +53,106 @@ public class BinaryTree {
 
         if (value == node.getValue()) {
             return true;
-        } else if(value < node.getValue()) {
+        } else if (value < node.getValue()) {
             return this.search(value, node.getLeft());
         } else {
             return this.search(value, node.getRight());
+        }
+    }
+
+    public boolean dfsSearch(int value) {
+        return this.dfsSearch(value, this.root);
+    }
+
+    private boolean dfsSearch(int value, TreeNode node) {
+        if (node == null) {
+            return false;
+        }
+
+        if (value == node.getValue()) {
+            return true;
+        }
+
+        if (this.dfsSearch(value, node.getLeft())) {
+            return true;
+        }
+
+        return this.dfsSearch(value, node.getRight());
+    }
+
+    public boolean bfsSearch(int value) {
+        if (this.root == null) {
+            return false;
+        }
+
+        Deque<TreeNode> deque = new ArrayDeque<>();
+
+        deque.addLast(root);
+
+        while (!deque.isEmpty()) {
+            TreeNode currentNode = deque.removeFirst();
+
+            if (currentNode.getValue() == value) {
+                return true;
+            }
+
+            if (currentNode.getLeft() != null) {
+                deque.addLast(currentNode.getLeft());
+            }
+
+            if (currentNode.getRight() != null) {
+                deque.addLast(currentNode.getRight());
+            }
+        }
+
+        return false;
+    }
+
+    public List<Integer> preorderTraversal() {
+        List<Integer> arr = new ArrayList<>();
+
+        this.preorderTraversal(arr, this.root);
+
+        return arr;
+    }
+
+    public List<Integer> inorderTraversal() {
+        List<Integer> arr = new ArrayList<>();
+
+        this.inorderTraversal(arr, this.root);
+
+        return arr;
+    }
+
+    public List<Integer> postorderTraversal() {
+        List<Integer> arr = new ArrayList<>();
+
+        this.postorderTraversal(arr, this.root);
+
+        return arr;
+    }
+
+    private void postorderTraversal(List<Integer> arr, TreeNode node) {
+        if (node != null) {
+            postorderTraversal(arr, node.getLeft());
+            postorderTraversal(arr, node.getRight());
+            arr.add(node.getValue());
+        }
+    }
+
+    private void inorderTraversal(List<Integer> arr, TreeNode node) {
+        if (node != null) {
+            inorderTraversal(arr, node.getLeft());
+            arr.add(node.getValue());
+            inorderTraversal(arr, node.getRight());
+        }
+    }
+
+    private void preorderTraversal(List<Integer> arr, TreeNode node) {
+        if (node != null) {
+            arr.add(node.getValue());
+            preorderTraversal(arr, node.getLeft());
+            preorderTraversal(arr, node.getRight());
         }
     }
 
--- binary_tree/Main.java
+++ binary_tree/Main.java
@@ -1,5 +1,7 @@
 package binary_tree;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree(new TreeNode(1));
@@ -15,8 +17,18 @@ public class Main {
         TreeNode root = binaryTree.getRoot();
         int number = 11;
         boolean hasInBinaryTree = binaryTree.search(number);
+        boolean hasInBinaryTreeDfs = binaryTree.dfsSearch(number);
+        boolean hasInBinaryTreeBfs = binaryTree.bfsSearch(number);
+        List<Integer> preorderTraversalArr = binaryTree.preorderTraversal();
+        List<Integer> inorderTraversalArr = binaryTree.inorderTraversal();
+        List<Integer> postorderTraversalArr = binaryTree.postorderTraversal();
 
         System.out.println(root);
         System.out.printf("The number %d has in binary tree? %b", number, hasInBinaryTree);
+        System.out.printf("%nThe number %d has in binary tree? (using dfs search) %b", number, hasInBinaryTreeDfs);
+        System.out.printf("%nThe number %d has in binary tree? (using bfs search) %b", number, hasInBinaryTreeBfs);
+        System.out.printf("%nThe array searches with preorder traversal %s", preorderTraversalArr.toString());
+        System.out.printf("%nThe array searches with inorder traversal %s", inorderTraversalArr.toString());
+        System.out.printf("%nThe array searches with postorder traversal %s", postorderTraversalArr.toString());
     }
 }
